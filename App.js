65126fb27209ad5abcf909d503ce1f3896f4ab48
Tatiane <30663303+tatianepinto@users.noreply.github.com>
2022-01-07 14:44:47 -0500

    Working on Add Contact

--- App.js
+++ App.js
@@ -1,20 +1,52 @@
+import * as React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer, createNavigationContainerRef } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AddContact from './components/addContact';
+import ContactsListScreen from './components/contactsListScreen';
+import { Component } from 'react';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const Stack = createNativeStackNavigator();
+const navigationRef = createNavigationContainerRef()
+
+function navigate(name, params) {
+  console.log("Navigate function");
+  console.log("navigationRef.isReady(): " + navigationRef.isReady());
+  if (navigationRef.isReady()) {
+    navigationRef.navigate(name, params);
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+class App extends Component {
+  
+  render() {
+    return (
+      <NavigationContainer  ref={navigationRef}>
+        <Stack.Navigator>
+          <Stack.Screen
+            name="Contacts"
+            component={ContactsListScreen}
+            options={{
+              // headerTitle: props => <LogoTitle {...props} />,
+              headerRight: () => (
+                <Button
+                  onPress={() => {
+                    navigate('AddContact');
+                    console.log("Pressed");
+                  }}
+
+                  title="+ Add"
+                  color="#f54242"
+                />
+              ),
+            }}
+          />
+          <Stack.Screen name="AddContact" component={AddContact} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+}
+
+export default App;
---
+++ components/addContact.js
@@ -0,0 +1,122 @@
+import * as React from 'react';
+import { Image, StyleSheet, TextInput, Text, View, TouchableOpacity, Button } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import * as ImagePicker from 'expo-image-picker';
+
+export default function AddContact() {
+  const [fName, onChangefName] = React.useState(null);
+  const [lName, onChangelName] = React.useState(null);
+  const [email, onChangeEmail] = React.useState(null);
+  const [number, onChangeNumber] = React.useState(null);
+  const [image, setImage] = React.useState(null);
+
+  const addImage = async () => {
+    let _image = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [4,3],
+      quality: 1,
+    });
+
+    if (!_image.cancelled) {
+      setImage(_image.uri);
+    }
+  }
+
+  return (
+    <View style={styles.container}>
+      <View style={imageUploaderStyles.container}>
+        {
+          image && <Image source={{ uri: image }} style={{ width: 200, height: 200 }} />
+        }
+
+        <View style={imageUploaderStyles.uploadBtnContainer}>
+          <TouchableOpacity onPress={addImage} style={imageUploaderStyles.uploadBtn} >
+            <Text>{image ? 'Edit' : 'Upload'} Image</Text>
+            <AntDesign name="camera" size={20} color="black" />
+          </TouchableOpacity>
+        </View>
+      </View>
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangefName}
+        value={fName}
+        placeholder="First Name"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangelName}
+        value={lName}
+        placeholder="Last Name"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeEmail}
+        value={email}
+        placeholder="Email"
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="Phone Number"
+        keyboardType="number"
+      />
+      <Button
+        style={styles.button}
+        onPress={onPressSave}
+        title="Save"
+        color="#f54242" />
+    </View>
+  );
+}
+
+const onPressSave = () => {
+  
+}
+
+const imageUploaderStyles = StyleSheet.create({
+  container: {
+    elevation: 2,
+    height: 200,
+    width: 200,
+    backgroundColor: '#efefef',
+    position: 'relative',
+    borderRadius: 999,
+    overflow: 'hidden',
+  },
+  uploadBtnContainer: {
+    opacity: 0.7,
+    position: 'absolute',
+    right: 0,
+    bottom: 0,
+    backgroundColor: 'lightgrey',
+    width: '100%',
+    height: '25%',
+  },
+  uploadBtn: {
+    display: 'flex',
+    alignItems: "center",
+    justifyContent: 'center'
+  }
+})
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  input: {
+    // minWidth: '20%',
+    width: 300,
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+  button: {
+    width: 500
+  }
+});
